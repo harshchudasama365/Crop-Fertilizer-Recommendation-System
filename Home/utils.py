1b2ff1663d8e268db8f49571e41be758c0df6b18
harshchudasama365 <harsh.chudasama@somaiya.edu>
2022-05-07 00:21:21 +0530

    fertilizer drop down added

--- Home/utils.py
+++ Home/utils.py
@@ -68,19 +68,19 @@ class CNN_Model(nn.Module):
 fertilizer_dict = {
         'NHigh': """<b style = "color:#c79c60;">The N value of soil is high and might give rise to weeds.</b>
         <br/><br/> Please consider the following suggestions:
-        <p align="justify">1. <i> Manure </i> – adding manure is one of the simplest ways to amend your soil with nitrogen. Be careful as there are various types of manures with varying degrees of nitrogen.</p>
+        <p align="justify">1. Plant ‘green manure’ crops like cabbage, corn and brocolli.</p>
         <p align="justify">2. <i>Coffee grinds </i> – use your morning addiction to feed your gardening habit! Coffee grinds are considered a green compost material which is rich in nitrogen. Once the grounds break down, your soil will be fed with delicious, delicious nitrogen. An added benefit to including coffee grounds to your soil is while it will compost, it will also help provide increased drainage to your soil.</p>
         <p align="justify">3. <i>Plant nitrogen fixing plants</i> – planting vegetables that are in Fabaceae family like peas, beans and soybeans have the ability to increase nitrogen in your soil</p>
-        <p align="justify">4. Plant ‘green manure’ crops like cabbage, corn and brocolli</p>
+        <p align="justify">4. <i> Manure </i> – adding manure is one of the simplest ways to amend your soil with nitrogen. Be careful as there are various types of manures with varying degrees of nitrogen.</p>
         <p align="justify">5. <i>Use mulch (wet grass) while growing crops</i> - Mulch can also include sawdust and scrap soft woods </p><hr style = "height:2px; background-color:#c79c60;">""",
 
         'Nlow': """<b style = "color:#c79c60;">The N value of your soil is low.</b>
         <br/><br/> Please consider the following suggestions:
-        <p align="justify"> 1. <i>Add sawdust or fine woodchips to your soil</i> – the carbon in the sawdust/woodchips love nitrogen and will help absorb and soak up and excess nitrogen.</p>
+        <p align="justify"> 1. Add composted manure to the soil.</p>
         <p align="justify">2. <i>Plant heavy nitrogen feeding plants</i> – tomatoes, corn, broccoli, cabbage and spinach are examples of plants that thrive off nitrogen and will suck the nitrogen dry.</p>
         <p align="justify">3. <i>Water</i> – soaking your soil with water will help leach the nitrogen deeper into your soil, effectively leaving less for your plants to use.</p>
         <p align="justify">4. <i>Sugar</i> – In limited studies, it was shown that adding sugar to your soil can help potentially reduce the amount of nitrogen is your soil. Sugar is partially composed of carbon, an element which attracts and soaks up the nitrogen in the soil. This is similar concept to adding sawdust/woodchips which are high in carbon content.</p>
-        <p align="justify">5. Add composted manure to the soil.</p>
+        <p align="justify">5. <i>Add sawdust or fine woodchips to your soil</i> – the carbon in the sawdust/woodchips love nitrogen and will help absorb and soak up and excess nitrogen.</p>
         <p align="justify">6. Plant Nitrogen fixing plants like peas or beans.</p>
         <p align="justify">7. <i>Use NPK fertilizers with high N value.</i></p>
         <p align="justify">8. <i>Do nothing</i> – It may seem counter-intuitive, but if you already have plants that are producing lots of foliage, it may be best to let them continue to absorb all the nitrogen to amend the soil for your next crops.</p><hr style = "height:2px; background-color:#c79c60;">""",
@@ -89,11 +89,11 @@ fertilizer_dict = {
 
         'PHigh': """<b style = "color:#c79c60;">The P value of your soil is high.</b>
         <br/> <br/>Please consider the following suggestions:
-        <p align="justify">1. <i>Avoid adding manure</i> – manure contains many key nutrients for your soil but typically including high levels of phosphorous. Limiting the addition of manure will help reduce phosphorus being added.</p>
+        <p align="justify">1. Use crop rotations to decrease high phosphorous levels</p>
         <p align="justify">2. <i>Use only phosphorus-free fertilizer</i> – if you can limit the amount of phosphorous added to your soil, you can let the plants use the existing phosphorus while still providing other key nutrients such as Nitrogen and Potassium. Find a fertilizer with numbers such as 10-0-10, where the zero represents no phosphorous.</p>
         <p align="justify">3. <i>Water your soil</i> – soaking your soil liberally will aid in driving phosphorous out of the soil. This is recommended as a last ditch effort.</p>
         <p align="justify">4. Plant nitrogen fixing vegetables to increase nitrogen without increasing phosphorous (like beans and peas).</p>
-        <p align="justify">5. Use crop rotations to decrease high phosphorous levels</p><hr style = "height:2px; background-color:#c79c60;">""",
+        <p align="justify">5. <i>Avoid adding manure</i> – manure contains many key nutrients for your soil but typically including high levels of phosphorous. Limiting the addition of manure will help reduce phosphorus being added.</p><hr style = "height:2px; background-color:#c79c60;">""",
 
         'Plow': """<b style = "color:#c79c60;">The P value of your soil is low.</b>
         <br/> <br/>Please consider the following suggestions:
@@ -121,11 +121,11 @@ fertilizer_dict = {
 
         'Klow': """<b style = "color:#c79c60;">The K value of your soil is low.</b>
         <br/><br/>Please consider the following suggestions:
-        <p align="justify">1. Mix in muricate of potash or sulphate of potash</p>
-        <p align="justify">2. Try kelp meal or seaweed</p>
-        <p align="justify">3. Try Sul-Po-Mag</p>
+        <p align="justify">1. Try kelp meal or seaweed</p>
+        <p align="justify">2. Mix in muricate of potash or sulphate of potash</p>
+        <p align="justify">3. Use Potash fertilizers since they contain high values potassium</p>
         <p align="justify">4. Bury banana peels an inch below the soils surface</p>
-        <p align="justify">5. Use Potash fertilizers since they contain high values potassium</p><hr style = "height:2px; background-color:#c79c60;">
+        <p align="justify">5. Try Sul-Po-Mag</p><hr style = "height:2px; background-color:#c79c60;">
         """,
 
         'KNo': """<b style = "color:#c79c60;">The K value of your soil is up to the mark.</b><hr style = "height:2px; background-color:#c79c60;"s>"""
